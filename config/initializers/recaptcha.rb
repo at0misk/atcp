c5dc30a167b57ca0dff7ad8185fb75e0176a6709
at0misk <ktp925@gmail.com>
2018-09-20 13:44:38 -0700

    fe

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,7 @@
+Recaptcha.configure do |config|
+  config.site_key  = '6LeLVXEUAAAAAI7ICtwyhwjSu7qq6Y8VXqzXDJEq'
+  config.secret_key = '6LeLVXEUAAAAAG36_EKNE-84Bp3hH3BJ7KPKsrxq
+'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
