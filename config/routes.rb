17956824714992bbb5969130590aed0554928da3
at0misk <ktp925@gmail.com>
2018-09-24 14:17:07 -0700

    fe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   post 'create' => 'sessions#create'
-  # get 'dl' => 'sessions#dl'
-  # resources :users
+  get 'dl' => 'sessions#dl'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
